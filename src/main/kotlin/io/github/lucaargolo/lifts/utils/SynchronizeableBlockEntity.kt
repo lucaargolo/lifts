e61ce3dd170e07ab8c0537fe2e51af534ee0f40f
lucaargolo <lucaargolo@gmail.com>
2021-12-02 07:35:16 -0300

    Update dependencies to 1.18 official release, fix block entities not properly syncing (Closes #28)

--- src/main/kotlin/io/github/lucaargolo/lifts/utils/SynchronizeableBlockEntity.kt
+++ src/main/kotlin/io/github/lucaargolo/lifts/utils/SynchronizeableBlockEntity.kt
@@ -3,10 +3,22 @@ package io.github.lucaargolo.lifts.utils
 import net.minecraft.block.BlockState
 import net.minecraft.block.entity.BlockEntity
 import net.minecraft.block.entity.BlockEntityType
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.network.Packet
+import net.minecraft.network.listener.ClientPlayPacketListener
+import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket
 import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.math.BlockPos
 
 open class SynchronizeableBlockEntity(type: BlockEntityType<*>, pos: BlockPos, state: BlockState): BlockEntity(type, pos, state) {
+
+    override fun toUpdatePacket(): Packet<ClientPlayPacketListener>? {
+        return BlockEntityUpdateS2CPacket.create(this) { NbtCompound().also(::writeNbt) }
+    }
+
+    override fun toInitialChunkDataNbt(): NbtCompound {
+        return NbtCompound().also(::writeNbt)
+    }
 
     fun sync() {
         ((this as? BlockEntity)?.world as? ServerWorld)?.chunkManager?.markForUpdate(this.pos)
